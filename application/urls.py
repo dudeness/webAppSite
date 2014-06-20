6df9899fd4c38158bc79e276fddb211227b0ae73
Andi Hanus <andi@donhu.net>
2014-06-20 20:53:09 +0200

    added static file handling in DEBUG = True In production cases static files should not be handled by django.

--- application/urls.py
+++ application/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 
+from application.settings import base as settings
 from application.modules.web import views as web_views
 
 admin.autodiscover()
@@ -12,6 +12,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
 
-    url(r'^$', web_views.DemoPage.as_view())
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes':True}),
+    )
+
+urlpatterns += patterns('',
+    url(r'^$', web_views.DemoPage.as_view()),
 )
