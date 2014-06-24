cf7d66e6652fc7d3fdeb705a8c91daf98b328ea0
Andi Hanus <andi@donhu.net>
2014-06-24 12:31:28 +0200

    initialize basic web module js environment

---
+++ application/modules/web/static/web/javascript/controller/web-controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('.web.controller', [])
+    .controller('webController', ['$scope',
+        function($scope) {
+            $scope.init = function() {
+
+            };
+        }]);
---
+++ application/modules/web/static/web/javascript/loader.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('#', [
+    'ngResource',
+    'ngAnimate',
+
+    '.web.controller'
+    ]);
