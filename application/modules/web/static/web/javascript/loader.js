8480e2393e09b66315557e4de925b2e8d267948c
Andi Hanus <andi@donhu.net>
2014-06-25 01:27:15 +0200

    set up dynamic angular module dependency loader for each INSTALLED_MODULE

--- application/modules/web/static/web/javascript/loader.js
+++ application/modules/web/static/web/javascript/loader.js
@@ -1,9 +1,18 @@
 'use strict';
 
-angular.module('#', [
+var dependencies = [
     'ngResource',
     'ngAnimate',
 
     '.web.controller',
     '.web.directive.section'
-    ]);
+];
+
+_.forEach(environment.modules, function(name) {
+    if (name !== 'web') {
+        dependencies.push('#' + name);
+    }
+
+});
+
+angular.module('#', dependencies);
